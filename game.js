581724694c627d1a4f0eba942cbf7db97c6213d0
qinlin891 <qinlin891@gmail.com>
2022-08-14 14:27:59 -0500

    Change to replay the game sequence from the start

--- game.js
+++ game.js
@@ -45,12 +45,17 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
-    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100)
-    playSound(randomChosenColour);
+    gamePattern.forEach((el, index) => {
+        setTimeout(function() {
+            $(`#${el}`).fadeIn(100).fadeOut(100).fadeIn(100)
+            playSound(el);
+        }, 500 * index)
+    })
 }
 
 function playSound(name) {
     let audio = new Audio(`sounds/${name}.mp3`);
+    audio.volume = 0.3;
     audio.play();
 }
 
